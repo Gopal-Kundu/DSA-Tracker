865d355cc2631544904c06125e06371f62259702
Gopal Kundu <kundugopal2005@gmail.com>
2026-09-07 20:30:21 +0530

    Spring boot inital setup added

---
+++ server/src/main/java/com/gopalkundu/leettracker/LeettrackerApplication.java
@@ -0,0 +1,13 @@
+package com.gopalkundu.leettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeettrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeettrackerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/gopalkundu/leettracker/LeettrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gopalkundu.leettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeettrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
